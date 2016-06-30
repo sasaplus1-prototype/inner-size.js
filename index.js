e35675a54448c1d090564de406962dc1e6b2ac4e
sasaplus1 <sasaplus1@gmail.com>
2016-06-30 10:20:20 +0900

    added license comment

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*!
+ * @license inner-size.js Copyright(c) 2016 sasa+1
+ * https://github.com/sasaplus1-prototype/inner-size.js
+ * Released under the MIT license.
+ */
 'use strict';
 
 function getHeight() {
